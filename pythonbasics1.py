b18ee6f3a8a2be70714360f24649ba060dbafe5b
Aiden Munger <=>
2026-01-21 10:48:57 -0800

    Added a bunch of lines of code showing comments and variables

--- pythonbasics1.py
+++ pythonbasics1.py
@@ -4,4 +4,19 @@
 
 import math, datetime, random as rn
 
+# Basic Print
 print("Hello, World!")
+
+"""
+This is a multi-line comment
+that spans several lines.
+"""
+
+# Variables
+
+x = 78
+y = 10.5
+
+x = "This is a string now"
+
+print(x, type(x))
