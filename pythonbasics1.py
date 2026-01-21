f2cc60d49d3f116dd0596b0b39a343e3849aa9c2
Aiden Munger <=>
2026-01-21 10:58:19 -0800

    Added section separators and improved formatting in pythonbasics1.py

--- pythonbasics1.py
+++ pythonbasics1.py
@@ -4,6 +4,7 @@
 
 import math, datetime, random as rn
 
+#################################################
 # Basic Print
 print("Hello, World!")
 
@@ -11,7 +12,7 @@ print("Hello, World!")
 This is a multi-line comment
 that spans several lines.
 """
-
+#################################################
 # Variables
 
 x = 78
@@ -25,6 +26,7 @@ print(x, type(x))
 x = True
 x = False
 
+###############################################
 # Operators
 
 x = 5 // 2
@@ -37,3 +39,19 @@ print(x, type(x))
 x = 3 ** 2
 
 print(x, type(x))
+
+###############################################
+# Math Module
+
+x = math.pow(3, 2)
+print(x)
+
+###############################################
+# Printing and Decimal Formatting
+
+x = 10.55678
+print(f"This is a string {x}")
+
+print(f"x = {x:.2f}")
+
+print(f"x = {round(x, 2)}")
